9113aaf12bd8e6562d138181b29640a08a5f3516
benJ <26485097+Mil00Z@users.noreply.github.com>
2024-08-14 11:23:39 +0200

    Scripts: Architecture JS + prepare coding flow

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+import { recipes } from "../datas/recipes.js";
+import { RecipeCard } from "./templates/recipeCard.js";
+
 const currentPage = 'home';
 
-if (document.body.classlist.contains(`${currentPage}`)) {
+if (document.body.classList.contains(`${currentPage}`)) {
 
         console.log(`on ${currentPage} - Page`);
+
+        console.log(recipes);
         
 }
